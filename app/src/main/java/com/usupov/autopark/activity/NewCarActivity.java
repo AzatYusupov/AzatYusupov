1c4cd3278c83b6c04f251a4dc81d5e4eae5d3391
Temur Usupov <yusupov.temur@gmail.com>
2017-01-29 14:09:34 +0500

    activity: new car

--- app/src/main/java/com/usupov/autopark/activity/NewCarActivity.java
+++ app/src/main/java/com/usupov/autopark/activity/NewCarActivity.java
@@ -1,12 +1,24 @@
 package com.usupov.autopark.activity;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.usupov.autopark.R;
 
+import java.util.ArrayList;
+
 public class NewCarActivity  extends AppCompatActivity {
+
+    protected static final int RESULT_SPEECH = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +27,41 @@ public class NewCarActivity  extends AppCompatActivity {
         setContentView(R.layout.activity_new_car);
 
         initToolbar();
+
+        final EditText edt = (EditText) findViewById(R.id.edittext_vin_number);
+        edt.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                final int DRAWABLE_LEFT = 0;
+                final int DRAWABLE_TOP = 1;
+                final int DRAWABLE_RIGHT = 2;
+                final int DRAWABLE_BOTTOM = 3;
+
+                if(event.getAction() == MotionEvent.ACTION_UP) {
+                    if(event.getRawX() >= (edt.getRight() - edt.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
+
+                        Intent intent = new Intent(
+                                RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, "en-US");
+
+                        try {
+                            startActivityForResult(intent, RESULT_SPEECH);
+                            EditText edt = (EditText) findViewById(R.id.edittext_vin_number);
+                            edt.setText("");
+                        } catch (ActivityNotFoundException a) {
+                            Toast t = Toast.makeText(getApplicationContext(),
+                                    "Opps! Your device doesn't support Speech to Text",
+                                    Toast.LENGTH_SHORT);
+                            t.show();
+                        }
+
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
 
     }
     /**
@@ -28,7 +75,29 @@ public class NewCarActivity  extends AppCompatActivity {
     }
 
     @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case RESULT_SPEECH: {
+                if (resultCode == RESULT_OK && null != data) {
+                    ArrayList<String> text = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+                    EditText edt = (EditText) findViewById(R.id.edittext_vin_number);
+                    edt.setText(text.toString());
+                }
+                break;
+            }
+
+        }
+
+    }
+
+    @Override
     public void onBackPressed() {
         super.onBackPressed();
     }
+
 }
